f998d5dd76c9e3037cd77dd0bd5c2258a2c0b327
Tanaka Kuma <kamyshek185@gmail.com>
2020-07-03 21:03:16 +0300

    Fix some problems

--- GameLibrary/SDL_Pixel.h
+++
@@ -1,9 +0,0 @@
-#include <SDL2/SDL.h>
-
-class SDL_Pixel
-{
-	public:
-	static int* GetPixel(SDL_Surface *surf, int x, int y);
-	static void SetPixel(SDL_Surface *surf, int x, int y, int* pix);
-	static SDL_Surface *ReturnMirrored(SDL_Surface * surf);
-};
